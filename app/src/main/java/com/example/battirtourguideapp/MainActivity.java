d3c2d6592af51a88344a92ffceef770775f39eca
Itaf Shakhtour <201603695@bethlehem.edu>
2020-02-11 15:30:51 +0200

    final

--- app/src/main/java/com/example/battirtourguideapp/MainActivity.java
+++ app/src/main/java/com/example/battirtourguideapp/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.battirtourguideapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageClickListener;
 import com.synnapps.carouselview.ImageListener;
 
-public class MainActivity extends AppCompatActivity {
 
 
+public class MainActivity extends AppCompatActivity
 
+
+    implements BottomNavigationView.OnNavigationItemSelectedListener {
+    private BottomNavigationView bottomNavigationView;
+
+
+    // hay ll slideshow
     private int[] mImages = new int[] {
             R.drawable.img1 ,
             R.drawable.img2 ,
@@ -34,7 +47,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        // button OnClich
+        Button arch = (Button) findViewById(R.id.arch);
+        Button torist =(Button) findViewById(R.id.tourist);
+        Button hotel =(Button) findViewById(R.id.hotel);
+        Button rest =(Button) findViewById(R.id.rest);
 
+arch.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent int1 = new Intent(MainActivity.this,arch.class);
+        startActivity(int1);
+    }
+});
+
+        hotel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int2 = new Intent(MainActivity.this,hotel.class);
+                startActivity(int2);
+            }
+        });
+
+
+        rest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int3 = new Intent(MainActivity.this,rest.class);
+                startActivity(int3);
+            }
+        });
+
+        torist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int4 = new Intent(MainActivity.this,tourist.class);
+                startActivity(int4);
+            }
+        });
+
+
+//had ll slideshow
         CarouselView carouselView = findViewById(R.id.carouselView);
         carouselView.setPageCount(mImages.length);
         carouselView.setImageListener(new ImageListener() {
@@ -54,5 +107,47 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
+        //ll navigation
+        bottomNavigationView = findViewById(R.id.nav_view);
+        setListeners();
+
     }
-}
+
+    private void setListeners() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+        //If item is Checked make it unchecked
+        if (item.isChecked())
+            item.setChecked(false);
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                //Check the Item
+
+                item.setChecked(true);
+                break;
+            case R.id.navigation_qr:
+                //Check the Item
+
+                item.setChecked(true);
+                break;
+            case R.id.navigation_map:
+                //Check the Item
+
+                item.setChecked(true);
+                break;
+        }
+        return true;
+    }
+
+
+
+
+    }
+
