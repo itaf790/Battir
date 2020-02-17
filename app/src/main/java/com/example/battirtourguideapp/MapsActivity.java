0115249a0e4f568e64b714f6305c8c247b0eec88
itaf790 <60775414+itaf790@users.noreply.github.com>
2020-02-17 21:40:32 +0200

    Update MapsActivity.java

--- app/src/main/java/com/example/battirtourguideapp/MapsActivity.java
+++ app/src/main/java/com/example/battirtourguideapp/MapsActivity.java
@@ -11,7 +11,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
