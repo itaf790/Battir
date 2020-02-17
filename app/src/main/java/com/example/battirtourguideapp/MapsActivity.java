630a16977754ef3a402c0549b76429be17407810
itaf790 <itafissam3@gmail.com>
2020-02-17 20:55:42 +0200

    last edit googel map done

--- app/src/main/java/com/example/battirtourguideapp/MapsActivity.java
+++ app/src/main/java/com/example/battirtourguideapp/MapsActivity.java
@@ -38,10 +38,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng (-34, 151);
-        mMap.addMarker (new MarkerOptions ().position (sydney).title ("Marker in Sydney"));
-        mMap.moveCamera (CameraUpdateFactory.newLatLng (sydney));
+        float zoom=12;
+        LatLng battir = new LatLng (31.729057,35.137903 );
+        mMap.addMarker (new MarkerOptions ().position (battir).title ("Marker in battir"));
+        mMap.moveCamera (CameraUpdateFactory.newLatLngZoom (battir,zoom));
     }
 }
