47b6175c59ca8cf395df1d761653f7e45d06eb6f
itaf790 <itafissam3@gmail.com>
2020-02-19 20:55:07 +0200

    MAP READY

--- app/src/main/java/com/example/battirtourguideapp/MainActivity.java
+++ app/src/main/java/com/example/battirtourguideapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.battirtourguideapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -128,6 +129,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavig
             }
             return false;
         }
+//about
 
 
     }
