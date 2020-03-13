822ab278ebef54c58b08c87cfa2a06410526e118
itaf790 <itafissam3@gmail.com>
2020-03-13 18:39:34 +0200

    13-3

--- app/src/main/java/com/example/battirtourguideapp/rest.java
+++ app/src/main/java/com/example/battirtourguideapp/rest.java
@@ -3,12 +3,41 @@ package com.example.battirtourguideapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toolbar;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class rest extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rest);
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_rest);
     }
+
+
 }
+
+
+
+
+
+
