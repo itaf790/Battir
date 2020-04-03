fcb2bda098ed8d0c4c4f4d2463285af95474126f
adawi <itaf790>
2020-04-04 01:02:28 +0300

    final

--- app/src/main/java/com/project/battirtourguideapp/tourist.java
+++ app/src/main/java/com/project/battirtourguideapp/tourist.java
@@ -1,14 +1,90 @@
 package com.project.battirtourguideapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
 
 public class tourist extends AppCompatActivity {
 
+    RecyclerView mRecyclerView;
+    FirebaseDatabase mFirebaseDatabase;
+    DatabaseReference mRef;
+    LinearLayoutManager mLayoutManager;
+    // Creating DatabaseReference.
+    DatabaseReference databaseReference;
+    // Creating StorageReference and DatabaseReference object.
+    StorageReference storageReference;
+
+    // Creating Progress dialog
+    ProgressDialog progressDialog;
+
+    ArrayList<ImageUploadInfo> imagesList;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_arch);
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_tourist);
+
+// Assign activity this to progress dialog.
+        progressDialog = new ProgressDialog (tourist.this);
+
+        // Setting up message in Progress dialog.
+        progressDialog.setMessage("Loading Images From Firebase.");
+
+        // Showing progress dialog.
+        progressDialog.show();
+
+        // Setting up Firebase image upload folder path in databaseReference.
+        // The path is already defined in MainActivity.
+        databaseReference = FirebaseDatabase.getInstance().getReference(MainActivity.Database_Path+"/Tourist Areas ");
+
+        imagesList = new ArrayList<ImageUploadInfo>();
+
+        // Adding Add Value Event Listener to databaseReference.
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+
+                    ImageUploadInfo imageUploadInfo = postSnapshot.getValue(ImageUploadInfo.class);
+
+                    imagesList.add(imageUploadInfo);
+                }
+
+                ListView listView = findViewById(R.id.list_view);
+
+                ImagesListAdapter adapter =
+                        new ImagesListAdapter(getApplicationContext(), R.layout.adapter_view_layout, imagesList);
+                listView.setAdapter(adapter);
+
+                // Hiding the progress dialog.
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+                // Hiding the progress dialog.
+                progressDialog.dismiss();
+
+            }
+        });
     }
 }
+
+
