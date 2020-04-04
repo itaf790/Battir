23099cc06399a40808499c177dc1d058d4027b9c
adawi <itaf790>
2020-04-04 21:38:58 +0300

    4-4

--- app/src/main/java/com/project/battirtourguideapp/ImageDetailsActivity.java
+++ app/src/main/java/com/project/battirtourguideapp/ImageDetailsActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
